e5e2df2bcb25960119b36fd9dd393cb76dd9de45
Karl Thomas Hauglid <thomas.hauglid@gmail.com>
2022-10-03 22:34:14 +0200

    Update text widget

--- coopx_design_system/lib/src/widgets/a_text.dart
+++ coopx_design_system/lib/src/widgets/a_text.dart
@@ -2,30 +2,47 @@ import 'package:coopx_design_system/src/shared/styles.dart';
 import 'package:flutter/material.dart';
 
 class AText extends StatelessWidget {
+  /// Fontsize 16
   AText.body(this.text, {Color? color, Key? key})
       : style = bodyStyle.copyWith(color: color),
         super(key: key);
 
+  /// Fontsize 18
+  AText.bodyBig(this.text, {Color? color, Key? key})
+      : style = bodyStyle.copyWith(color: color, fontSize: 18),
+        super(key: key);
+
+  /// Fontsize 14
+  AText.bodySmall(this.text, {Color? color, Key? key})
+      : style = bodyStyle.copyWith(color: color, fontSize: 14),
+        super(key: key);
+
+  /// Fontsize 12
   AText.caption(this.text, {Color? color, Key? key})
       : style = captionStyle.copyWith(color: color),
         super(key: key);
 
+  /// Fontsize 34
   AText.headingOne(this.text, {Color? color, Key? key})
       : style = heading1Style.copyWith(color: color),
         super(key: key);
 
-  AText.headingThree(this.text, {Color? color, Key? key})
-      : style = heading3Style.copyWith(color: color),
-        super(key: key);
-
+  /// Fontsize 28
   AText.headingTwo(this.text, {Color? color, Key? key})
       : style = heading2Style.copyWith(color: color),
         super(key: key);
 
+  /// Fontsize 24
+  AText.headingThree(this.text, {Color? color, Key? key})
+      : style = heading3Style.copyWith(color: color),
+        super(key: key);
+
+  /// Fontsize 30
   AText.headline(this.text, {Color? color, Key? key})
       : style = headlineStyle.copyWith(color: color),
         super(key: key);
 
+  /// Fontsize 20
   AText.subheading(this.text, {Color? color, Key? key})
       : style = subheadingStyle.copyWith(color: color),
         super(key: key);
